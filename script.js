4c538ffa803eb2c5cd1566f2e0bf3f4446f11333
dibkb <dibas9110@gmail.com>
2023-03-23 13:02:05 +0530

    +2

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const person = new Object({
-  name: "John Doe",
-  proession: "Teacher",
-  age: 43,
-  set updateName(newname) {
-    this.name = newname;
-  },
-});
-let content = "";
-for (x in person) {
-  content += person[x] + " ";
+// const person = new Object({
+//   name: "John Doe",
+//   proession: "Teacher",
+//   age: 43,
+//   set updateName(newname) {
+//     this.name = newname;
+//   },
+// });
+// let content = "";
+// for (x in person) {
+//   content += person[x] + " ";
+// }
+// const personArr = Object.values(person);
+// const personJson = JSON.stringify(person);
+// person.updateName = "John Cena";
+// document.getElementById("person").innerHTML = person.name;
+// console.log(personJson);
+// --------------------------- CONSTRUCTOR--------------------------------------
+function Person(firstName, lastName, age, profession) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.profession = profession;
+  this.changeName = function (newFirstName, newLastName) {
+    this.firstName = newFirstName;
+    this.lastName = newLastName;
+  };
 }
-const personArr = Object.values(person);
-const personJson = JSON.stringify(person);
-person.updateName = "John Cena";
-document.getElementById("person").innerHTML = person.name;
-console.log(personJson);
+const personOne = new Person("John", "Doe", 34, "Terrorist");
+const personTwo = new Person("Sally", "Rose", 41, "Feminist");
+console.log(personOne);
+personOne.changeName("Rowdy", "Rathore");
+console.log(personOne);

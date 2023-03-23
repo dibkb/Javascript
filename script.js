5622c40f12abb595abff4cba7c47b2e2e095a28e
dibkb <dibas9110@gmail.com>
2023-03-23 13:19:06 +0530

    +map

--- script.js
+++ script.js
@@ -16,18 +16,36 @@
 // document.getElementById("person").innerHTML = person.name;
 // console.log(personJson);
 // --------------------------- CONSTRUCTOR--------------------------------------
-function Person(firstName, lastName, age, profession) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.profession = profession;
-  this.changeName = function (newFirstName, newLastName) {
-    this.firstName = newFirstName;
-    this.lastName = newLastName;
-  };
-}
-const personOne = new Person("John", "Doe", 34, "Terrorist");
-const personTwo = new Person("Sally", "Rose", 41, "Feminist");
-console.log(personOne);
-personOne.changeName("Rowdy", "Rathore");
-console.log(personOne);
+// function Person(firstName, lastName, age, profession) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.profession = profession;
+//   this.changeName = function (newFirstName, newLastName) {
+//     this.firstName = newFirstName;
+//     this.lastName = newLastName;
+//   };
+// }
+// const personOne = new Person("John", "Doe", 34, "Terrorist");
+// const personTwo = new Person("Sally", "Rose", 41, "Feminist");
+// console.log(personOne);
+// personOne.changeName("Rowdy", "Rathore");
+// console.log(personOne);
+// --------------------------------SET--------------------------------------
+// const letters = new Set(["a", "b", "c", "c"]);
+// letters.add("g");
+// letters.add("gg");
+// letters.add("g");
+// letters.forEach((val, inedx) => {
+//   console.log(inedx);
+// });
+// console.log(letters.values());
+// --------------------------------MAPS--------------------------------------
+const cars = new Map();
+cars.set("bmw", 4000);
+cars.set("audi", 4000);
+cars.set("lincoln", 5000);
+cars.forEach((car, index) => {
+  console.log(car);
+  console.log("index", index);
+});

4f97a5886412e6e89e7feb86d04e73559ae1ffa6
dibkb <dibas9110@gmail.com>
2023-03-26 09:26:09 +0530

    cb

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const request = fetch("https://restcountries.com/v3.1/name/usa");
-request
-  .then((res) => res.json())
-  .then((json) => {
-    console.log(json);
-  });
+// const request = fetch("https://restcountries.com/v3.1/name/usa");
+// request
+//   .then((res) => res.json())
+//   .then((json) => {
+//     console.log(json);
+//   });
+const display = (val) => {
+  const displayDiv = document.getElementById("display");
+  displayDiv.innerHTML = val;
+};
+const calcluate = (numOne, numTwo, cb) => {
+  const sum = numOne + numTwo;
+  cb(sum);
+};
+// calcluate(5, 4, display);

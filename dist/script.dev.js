d1f203d3fb164e6e8281f626384bc406583fbf52
dibkb <dibas9110@gmail.com>
2023-03-27 20:25:52 +0530

    mouseevents

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -5,4 +5,12 @@ var zack = document.querySelector("person");
 var yash = document.querySelector("div.yash");
 yash.style.width = "600px";
 yash.style.height = "600px";
+yash.style.fontSize = "24px";
 yash.style.backgroundImage = "url(https://images.pexels.com/photos/7175450/pexels-photo-7175450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
+yash.addEventListener("mouseover", function () {
+  console.log("first");
+  this.innerHTML = "MOUSE HOVER";
+});
+yash.addEventListener("mouseout", function () {
+  this.innerHTML = "Thankyou";
+});
--- script.js
+++ script.js
@@ -3,5 +3,13 @@ const zack = document.querySelector("person");
 const yash = document.querySelector("div.yash");
 yash.style.width = "600px";
 yash.style.height = "600px";
+yash.style.fontSize = "24px";
 yash.style.backgroundImage =
   "url(https://images.pexels.com/photos/7175450/pexels-photo-7175450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
+yash.addEventListener("mouseover", function () {
+  console.log("first");
+  this.innerHTML = "MOUSE HOVER";
+});
+yash.addEventListener("mouseout", function () {
+  this.innerHTML = "Thankyou";
+});

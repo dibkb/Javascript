42b2a444f435f1e2f5fc597669b00dce8070ba96
dibkb <dibas9110@gmail.com>
2023-03-27 20:16:41 +0530

    manupulating style

---
+++ dist/script.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var divs = document.getElementsByTagName("div");
+var zack = document.querySelector("person");
+var yash = document.querySelector("div.yash");
+yash.style.width = "600px";
+yash.style.height = "600px";
+yash.style.backgroundImage = "url(https://images.pexels.com/photos/7175450/pexels-photo-7175450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
--- script.js
+++ script.js
@@ -1,58 +1,7 @@
-// class Cars {
-//   constructor(make, speed) {
-//     this.make = make;
-//     this.speed = speed;
-//   }
-//   accelerate() {
-//     this.speed += 10;
-//     console.log(`${this.make} is going at ${this.speed} km/hr`);
-//   }
-//   brake() {
-//     this.speed -= 5;
-//     console.log(`${this.make} is going at ${this.speed} km/hr`);
-//   }
-//   get speedUS() {
-//     return this.speed / 1.6;
-//   }
-//   set speedUS(val) {
-//     this.speed = 1.6 * val;
-//   }
-// }
-// const ford = new Cars("FORD", 120);
-// console.log(ford.speedUS);
-// ford.accelerate();
-// ford.accelerate();
-// ford.brake();
-// ford.speedUS = 50;
-// console.log(ford);
-
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/hr`);
-};
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed} km/hr`);
-};
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
-EV.prototype = Object.create(Car.prototype);
-EV.prototype.constructor = EV;
-EV.prototype.chargeBattery = function (val) {
-  this.charge = val;
-};
-EV.prototype.accelerate = function (val) {
-  this.speed += 10;
-  this.charge--;
-  console.log(`${this.make} is going at ${this.speed} km/hr`);
-};
-const tesla = new EV("tesla", 120, 34);
-tesla.chargeBattery(90);
-tesla.accelerate();
-console.log(tesla);
+const divs = document.getElementsByTagName("div");
+const zack = document.querySelector("person");
+const yash = document.querySelector("div.yash");
+yash.style.width = "600px";
+yash.style.height = "600px";
+yash.style.backgroundImage =
+  "url(https://images.pexels.com/photos/7175450/pexels-photo-7175450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";

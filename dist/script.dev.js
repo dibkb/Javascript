ca80faf8098d2c3000b08d1c5a374e67b75fc61b
dibkb <dibas9110@gmail.com>
2023-03-27 20:37:17 +0530

    appending

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,16 +1,22 @@
 "use strict";
 
+var body = document.querySelector("body");
 var divs = document.getElementsByTagName("div");
-var zack = document.querySelector("person");
+var zack = document.getElementById("person");
 var yash = document.querySelector("div.yash");
 yash.style.width = "600px";
 yash.style.height = "600px";
 yash.style.fontSize = "24px";
 yash.style.backgroundImage = "url(https://images.pexels.com/photos/7175450/pexels-photo-7175450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
 yash.addEventListener("mouseover", function () {
-  console.log("first");
   this.innerHTML = "MOUSE HOVER";
 });
 yash.addEventListener("mouseout", function () {
   this.innerHTML = "Thankyou";
 });
+body.removeChild(zack);
+var ironman = document.createElement("div");
+var node = document.createTextNode("Tony Stark");
+ironman.appendChild(node);
+body.appendChild(ironman);
+console.log(ironman);
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const body = document.querySelector("body");
 const divs = document.getElementsByTagName("div");
-const zack = document.querySelector("person");
+const zack = document.getElementById("person");
 const yash = document.querySelector("div.yash");
 yash.style.width = "600px";
 yash.style.height = "600px";
@@ -7,9 +8,14 @@ yash.style.fontSize = "24px";
 yash.style.backgroundImage =
   "url(https://images.pexels.com/photos/7175450/pexels-photo-7175450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
 yash.addEventListener("mouseover", function () {
-  console.log("first");
   this.innerHTML = "MOUSE HOVER";
 });
 yash.addEventListener("mouseout", function () {
   this.innerHTML = "Thankyou";
 });
+body.removeChild(zack);
+const ironman = document.createElement("div");
+const node = document.createTextNode("Tony Stark");
+ironman.appendChild(node);
+body.appendChild(ironman);
+console.log(ironman);
